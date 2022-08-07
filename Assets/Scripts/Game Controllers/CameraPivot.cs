50d5d194b34b5f0da363b7fe1451e751618bc2e2
PetarSimonovic <petar@simonovic.com>
2022-08-07 09:27:59 +0100

    Creates grid

--- Assets/Scripts/Game Controllers/CameraPivot.cs
+++ Assets/Scripts/Game Controllers/CameraPivot.cs
@@ -5,9 +5,13 @@ using UnityEngine;
 public class CameraPivot : MonoBehaviour
 {
     private Vector3 isometricRotation = new Vector3(30, 45, 0);
+    private Vector3 hiveCrashRotation = new Vector3(30, 0, 0);
+    private Vector3 hiveCrashPosition = new Vector3(2, 5, 0);
+
 
     public void Awake()
     {
-      transform.eulerAngles = isometricRotation;
+      transform.eulerAngles = hiveCrashRotation;
+      transform.position = hiveCrashPosition;
     }
 }
--- Assets/Scripts/Map/MapCreator.cs
+++ Assets/Scripts/Map/MapCreator.cs
@@ -8,10 +8,10 @@ public class MapCreator : MonoBehaviour
     [SerializeField]
     private GameObject tilePrefab;
 
-    private const float WidthOfTile = 0.8f;
+    private const float WidthOfTile = 1f;
     private const float StartingXPosition = 0f;
     private const float StartingZPosition = 0f;
-    private const int NumberOfRows = 10;
+    private const int NumberOfRows = 30;
     private const int NumberOfColumns = 10;
 
     public void Awake()
@@ -20,32 +20,33 @@ public class MapCreator : MonoBehaviour
     }
 
 
-    public GameObject CreateTile(Vector3 position)
-    {
-      GameObject tile = Instantiate(tilePrefab, position, Quaternion.identity);
-      tile.SetActive(true);
-      return tile;
-    }
-
     public void CreateMap()
     {
       float xPosition = StartingXPosition;
       float zPosition = StartingZPosition;
       for(int i = 0; i < NumberOfColumns; i ++)
       {
-        CreateRow(xPosition, zPosition);
-        xPosition -= WidthOfTile/2;
-        zPosition -= WidthOfTile/4;;
+        CreateColumn(xPosition, zPosition);
+        xPosition += WidthOfTile/2;
+        zPosition = zPosition == 0 ? WidthOfTile/4 : 0;
       }
 
     }
 
-    public void CreateRow(float xPosition, float zPosition)
+    public void CreateColumn(float xPosition, float zPosition)
     {
       for (float i = 0; i < NumberOfRows; i++)
       {
         CreateTile(new Vector3(xPosition, 0, zPosition));
-        zPosition -= WidthOfTile/2;
+        zPosition += WidthOfTile/2;
       }
+    }
+
+
+    public GameObject CreateTile(Vector3 position)
+    {
+      GameObject tile = Instantiate(tilePrefab, position, Quaternion.identity);
+      tile.SetActive(true);
+      return tile;
     }
 }

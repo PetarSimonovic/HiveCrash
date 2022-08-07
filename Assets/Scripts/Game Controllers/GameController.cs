f33f090a44741ba7ac906e5781f8ba3bc306ace4
PetarSimonovic <petar@simonovic.com>
2022-08-07 11:21:24 +0100

    Tiles can be clicked on; tile colliders tweaked

--- Assets/Scripts/Game Controllers/GameController.cs
+++ Assets/Scripts/Game Controllers/GameController.cs
@@ -15,6 +15,23 @@ public class GameController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+      checkInput();
+    }
 
+    private void checkInput()
+    {
+
+      if (Input.GetMouseButtonDown(0))
+      {
+        Ray raycast = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+        RaycastHit raycastHit;
+        if (Physics.Raycast(raycast, out raycastHit))
+        {
+          GameObject tile = raycastHit.transform.gameObject;
+          Debug.Log(tile);
+          Destroy(tile);
+         }
+
+      }
     }
 }

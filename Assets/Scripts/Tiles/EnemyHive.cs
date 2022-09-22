c853200c74edc448e1b8789f458e57a333ce313e
PetarSimonovic <petar@simonovic.com>
2022-09-22 19:33:48 +0100

    further collidertile inheritance

---
+++ Assets/Scripts/Tiles/EnemyHive.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHive : ColliderTile
+{
+    GameObject enemyHive;
+
+    protected override void reveal()
+    {
+        base.reveal();
+        
+    }
+}

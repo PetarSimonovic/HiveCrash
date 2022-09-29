9ee7b47d963a6f4d48a9b6c8a72fd90d80ef0db5
PetarSimonovic <petar@simonovic.com>
2022-09-29 20:31:42 +0100

    UI text added; textmeshpro imported

---
+++ Assets/Scripts/UI/UIText.cs
@@ -0,0 +1,22 @@
+using TMPro;
+using UnityEngine;
+
+
+public class UIText : MonoBehaviour
+{
+    [SerializeField]
+    private TMP_Text hivePollenUI;
+
+    private int hivePollen;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hivePollenUI.SetText("It works!");
+    }
+}

50486d82a72afd824789ef91b026b5df8530e670
PetarSimonovic <petar@simonovic.com>
2023-03-28 09:14:28 +0100

    Mouse controls

--- Assets/Scripts/Game Controllers/GameController.cs
+++ Assets/Scripts/Game Controllers/GameController.cs
@@ -91,9 +91,8 @@ public class GameController : MonoBehaviour
   
     private void checkGameInput()
     {
-      if (Input.touchCount > 0)
+      if (Input.GetMouseButton(0) || Input.GetMouseButtonDown(0) || Input.GetMouseButtonUp(0))
       {
-        touch = Input.GetTouch(0);
           processInput();
       }
     }
@@ -107,7 +106,7 @@ public class GameController : MonoBehaviour
         }
      
    
-      Vector3 touchPosition = touch.position;
+      Vector3 touchPosition = Input.mousePosition;
       Ray raycast = cameraController.GetCamera().ScreenPointToRay(touchPosition);
       Vector3 worldTouchPoint = cameraController.GetCamera().ScreenToWorldPoint(touchPosition);
       RaycastHit raycastHit;
@@ -143,17 +142,19 @@ public class GameController : MonoBehaviour
 
     private void processMapInput(Vector3 tilePosition, Vector3 worldTouchPoint)
     {
-      switch(touch.phase)
+      if (Input.GetMouseButtonDown(0))
       {
-      case TouchPhase.Began:
         processTouchOnMap(tilePosition);
-        break;
-      case TouchPhase.Moved:
+        return;
+      }
+      if (Input.GetMouseButton(0)) 
+      {
           beeLauncher.SetEndPosition(worldTouchPoint);
-        break;
-      case TouchPhase.Ended:
+        return;
+      }
+      if (Input.GetMouseButtonUp(0)) 
+      {
           beeLauncher.LaunchBee();
-        break;
       }
     }
 
